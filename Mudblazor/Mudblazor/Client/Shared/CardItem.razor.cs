0da5874a47f92d0cbb9a217e381fabc83de61b41
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-23 14:06:32 +0000

    Card examples

---
+++ Mudblazor/Mudblazor/Client/Shared/CardItem.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.RegularExpressions;
+
+namespace Mudblazor.Client.Shared
+{
+    public partial class CardItem
+    {
+        private Regex RegInitials = new Regex(@"(\b[a-zA-Z])[a-zA-Z]* ?");
+
+        [Parameter]
+        public string PostedBy { get; set; }
+        [Parameter]
+        public string Location { get; set; }
+        [Parameter]
+        public string ImageAddress { get; set; }
+        [Parameter]
+        public string Description { get; set; }
+
+
+        public string Initials => RegInitials.Replace(PostedBy, "$1");
+    }
+}
