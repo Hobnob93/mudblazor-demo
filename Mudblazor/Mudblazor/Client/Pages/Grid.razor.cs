8149e172f3b6494b2dc8dcba23e924c1b8951b17
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-23 11:38:26 +0000

    Grid layout page done

---
+++ Mudblazor/Mudblazor/Client/Pages/Grid.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Mudblazor.Client.Pages
+{
+    public partial class Grid
+    {
+        public int Spacing { get; set; } = 2;
+
+        void AddSpacing()
+        {
+            Spacing++;
+            Spacing %= 10;
+
+            StateHasChanged();
+        }
+    }
+}
--- Mudblazor/Mudblazor/Server/Controllers/ThemeController.cs
+++ Mudblazor/Mudblazor/Server/Controllers/ThemeController.cs
@@ -21,7 +21,7 @@ namespace Mudblazor.Server.Controllers
                         Black = "#27272f",
                         Background = "#32333d",
                         BackgroundGrey = "#27272f",
-                        Surface = "#373740",
+                        Surface = "#2f2f37",
                         DrawerBackground = "#27272f",
                         DrawerText = "rgba(255, 255, 255, 0.50)",
                         DrawerIcon = "rgba(255, 255, 255, 0.50)",
