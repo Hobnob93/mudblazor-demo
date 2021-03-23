0e8f0b386002874fdf0650368d0fcc58303e0936
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-23 14:44:48 +0000

    Dialog and custom message boxes

---
+++ Mudblazor/Mudblazor/Client/Pages/Cards.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Mudblazor.Client.Shared;
+using MudBlazor;
+using System.Threading.Tasks;
+
+namespace Mudblazor.Client.Pages
+{
+    public partial class Cards
+    {
+        [Inject]
+        public IDialogService Dialog { get; set; }
+
+        public MudMessageBox MBox { get; set; }
+        public string MessageBoxResult { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            MessageBoxResult = "No message box response yet.";
+        }
+
+        public void ShowDialog()
+        {
+            Dialog.Show<SampleDialog>("Sample Dialog");
+        }
+
+        public async Task ShowDefaultMessageBox()
+        {
+            var result = await Dialog.ShowMessageBox(
+                "Warning",
+                "Deleting can not be undone!",
+                yesText: "Delete!", cancelText: "Cancel");
+            MessageBoxResult = result == null ? "Cancelled" : "Deleted!";
+        }
+
+        public async Task ShowMessageBox()
+        {
+            bool? result = await MBox.Show();
+            MessageBoxResult = result == null ? "Cancelled" : "Deleted!";
+        }
+    }
+}
---
+++ Mudblazor/Mudblazor/Client/Shared/SampleDialog.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Mudblazor.Client.Shared
+{
+    public partial class SampleDialog
+    {
+        [CascadingParameter]
+        public MudDialogInstance MudDialog { get; set; }
+
+
+        void Submit() => MudDialog.Close(DialogResult.Ok(true));
+        void Cancel() => MudDialog.Cancel();
+    }
+}
